a92e1fb07c24465e88e452208bd0daf1b6246dbc
sumetpatil <sumetpatil@microsoft.com>
2024-04-05 16:43:40 -0600

    local_vnet_peering_script_updated

---
+++ Re-sync_vnet_peering/script_to_sync_vnet_peering_local_tenant.ps1
@@ -0,0 +1,55 @@
+﻿#Connecting to the account where you want to perform the task
+Write-Output ("Please connect to the correct Az Account once the browser pops up with your account")
+Connect-AzAccount 																			
+Write-Output ("Connected to the Account")
+
+#Getting the list of the Subscriptions in your tenant
+$Subscriptions = $(Get-AzSubscription | Where-Object { ($_.Name.StartsWith("Microsoft")) -and ($_.Name -match $Environment) -and (($_.State -ieq "Enabled") -or ($_.State -ieq "Active")) })
+
+# Checking all the subscription one by one in the list by selecting them
+foreach ($Subscription in $Subscriptions) {
+    Select-AzSubscription -SubscriptionId $Subscription.Id | Out-Null
+    Set-AzContext -Subscription $Subscription.Id | Out-Null
+
+    #Get all ARM resources from all resource groups
+    $ResourceGroups = Get-AzResourceGroup
+    foreach ($ResourceGroup in $ResourceGroups) {
+       	
+        $Resources = Get-AzResource -ResourceGroupName $ResourceGroup.ResourceGroupName
+        foreach ($Resource in $Resources) {
+            if ($Resource.ResourceType -eq "Microsoft.Network/virtualNetworks" ) {
+            	
+                #Pulling the Vnet peering information
+                $VirtualNetworkPeerings = Get-AzVirtualNetworkPeering -VirtualNetworkName $Resource.Name -ResourceGroupName $ResourceGroup.ResourceGroupName
+                foreach ($VirtualNetworkPeering in $VirtualNetworkPeerings) {
+                    Write-Output ("SubscriptionName:                  " + $Subscription.Name)
+                    Write-Output ("SubscriptionId:                    " + $Subscription.Id)
+                    Write-Output ("SubscriptionState:                 " + $Subscription.State)
+                    Write-Output ("ResourceGroup:                     " + $ResourceGroup.ResourceGroupName)
+                    Write-Output ("ResourceType:                      " + $Resource.ResourceType)
+                    Write-Output ("ResourceName:                      " + $Resource.Name)
+                    Write-Output ("VirtualNetworkPeeringName:         " + $VirtualNetworkPeering.Name)
+                    Write-Output ("VirtualNetworkPeeringState:        " + $VirtualNetworkPeering.PeeringState )
+                    Write-Output ("VirtualNetworkPeeringSyncLevel:    " + $VirtualNetworkPeering.peeringSyncLevel )
+		    
+                    # checking the peeering status to re-sync the peering 
+                    if ($VirtualNetworkPeering.peeringSyncLevel -contains "LocalNotInSync") {
+                
+                        Write-Output ("VirtualNetworkPeeringRemoteVirtualNetwork:    " + $VirtualNetworkPeering.RemoteVirtualNetworkText )                         
+                        Write-Output ("                                   " + "RESYNCing......" )
+                        Sync-AzVirtualNetworkPeering -Name $VirtualNetworkPeering.Name -VirtualNetworkName $Resource.Name -ResourceGroupName $ResourceGroup.ResourceGroupName
+                        Write-Output ("                                   " + "RESYNCing......" )
+
+                    }
+                    elseif ($VirtualNetworkPeering.peeringSyncLevel -contains "RemoteNotInSync") {
+                        Write-Output ("Please check the remote peering as this Tenant does not have any subscription with the LocalNotInSync status " )
+                    }
+                    else {
+                        Write-Output ("No resources were found with the LocalNotInSync status everything is updated" )
+                    }
+                }
+            }
+        }
+    }
+}
+Write-Output ("Script ended")
