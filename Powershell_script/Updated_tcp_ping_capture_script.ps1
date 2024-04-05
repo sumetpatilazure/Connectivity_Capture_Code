eac8d6f2e1bdc9fdd1b227f7009ff029c2df36f7
sumetpatil <sumetpatil@microsoft.com>
2024-04-05 16:47:28 -0600

    tnc_script_updated

--- Powershell_script/Updated_tcp_ping_capture_script.ps1
+++ Powershell_script/Updated_tcp_ping_capture_script.ps1
@@ -7,22 +7,30 @@ $Interval = Read-Host -prompt 'Enter the number of seconds between retries'
 $Filepath = Read-Host -prompt 'Enter the file path where you want to save the file'
 
 # Run try and catch to handle the exceptions gracefully when the script is stopped
-Try {
-	$pcappath = Read-Host -prompt 'Enter the file path where you want to save the pcap file' 													# File name to capture the sessions
-	netsh trace start capture=yes tracefile=$pcappath.etl maxsize=500 filemode=circular report=no													# Capturing the sessions starts here
+Try {	
+	# File name to capture the sessions
+	$pcappath = Read-Host -prompt 'Enter the file path where you want to save the pcap file'
+	# Capturing the sessions starts here 													
+	netsh trace start capture=yes tracefile=$pcappath.etl maxsize=500 filemode=circular report=no													
         if($FQDN){
-     		Get-TimeZone | Out-File $Filepath -Append																		# Loop with conditions to run the TCP connections
+		
+     		Get-TimeZone | Out-File $Filepath -Append
+		# Loop with conditions to run the TCP connections																		
         	while($True){
-			Test-NetConnection -ComputerName $FQDN -Port $port_numner | Out-File $Filepath -Append												# capturing the time in the UTC format
+			Test-NetConnection -ComputerName $FQDN -Port $port_numner | Out-File $Filepath -Append	
+			# capturing the time in the UTC format											
         		"UTC TCP Ping Time $((get-date).ToUniversalTime().ToString("yyyy/MM/dd HH:mm:ss:ffffff"))" | Out-File $Filepath -Append
         		Start-Sleep -Seconds $Interval
-        		Write-Output $arg | Out-File $Filepath -Append																	# Appending the output to the file
-        		$count+=1																					# Counting the number of the sessions
+			# Appending the output to the file
+        		Write-Output $arg | Out-File $Filepath -Append
+			# Counting the number of the sessions																	
+        		$count+=1																					
 	        	$count_number = Write-Output "####################################################'TCP_PING_NUMBER = $count'################################################################"
         		$count_number | Out-File $Filepath -Append
 	        	 }
           	}
- 	elseif($IP_address){																						# Repeating the above if IP address is entered instead of FQDN
+ 	elseif($IP_address){	
+		# Repeating the above if IP address is entered instead of FQDN																					
     		Get-TimeZone | Out-File $Filepath -Append
 		while($True){
 			Test-NetConnection $IP_address -Port $port_numner
@@ -47,7 +55,7 @@ catch{
 	}
 
 finally{   
-	   
+	   # stopping the capture
 	   $netsh_stop = netsh trace stop
-	   $netsh_stop | Out-File $Filepath -Append																		         # stopping the capture
+	   $netsh_stop | Out-File $Filepath -Append																		        
 	}
